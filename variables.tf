2bc561326739e76420ec289182814bc03459e8c6
Alexandros Kazantzidis <8426315+akazantzidis@users.noreply.github.com>
2023-05-20 19:05:58 +0300

    added description to variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "ha_control_plane" {
-  type    = bool
-  default = false
+  description = "Set HA for K3S control plane.If HA == true then 3 master nodes are provisioned."
+  type        = bool
+  default     = false
 }
 
 variable "cluster_name" {
-  type    = string
-  default = "default-cluster"
+  description = "The clusters name."
+  type        = string
+  default     = "default-cluster"
 }
 
 variable "masters" {
+  description = "The master node configuration options"
   type = object({
     name      = optional(string, "control")
     node      = optional(string, "")
@@ -42,10 +45,12 @@ variable "masters" {
 }
 
 variable "proxmox_nodes" {
-  type = list(string)
+  description = "List of proxmox nodes availiable for k3s nodes setup."
+  type        = list(string)
 }
 
 variable "pools" {
+  description = "Worker pool configuration options"
   type = list(object({
     name      = optional(string, "node")
     workers   = optional(number, 1)
@@ -80,33 +85,39 @@ variable "pools" {
 }
 
 variable "cloudinit_search_domain" {
-  type    = string
-  default = null
+  description = "VM DNS domain search"
+  type        = string
+  default     = null
 }
 
 variable "cloudinit_nameserver" {
-  type    = string
-  default = null
+  description = "VM DNS server"
+  type        = string
+  default     = null
 }
 
 variable "vm_cpu_type" {
-  type    = string
-  default = "host"
+  description = "VM CPU type"
+  type        = string
+  default     = "host"
 }
 
 variable "vm_sockets" {
-  type    = number
-  default = 1
+  description = "VM CPU sockets"
+  type        = number
+  default     = 1
 }
 
 variable "vm_boot" {
-  type    = string
-  default = "order=virtio0"
+  description = "VM boot device configuration"
+  type        = string
+  default     = "order=virtio0"
 }
 
 variable "subnet" {
-  type    = string
-  default = "192.168.1.1/24"
+  description = ""
+  type        = string
+  default     = "192.168.1.1/24"
 
   validation {
     condition     = can(cidrsubnet(var.subnet, 0, 0))
@@ -125,114 +136,136 @@ variable "gw" {
 }
 
 variable "k3s_master_kubelet_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true"]
+  description = "K3s master kubelet arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true"]
 }
 
 variable "k3s_worker_kubelet_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true"]
+  description = "K3s worker kubelet arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true"]
 }
 
 variable "k3s_kube_control_manag_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
+  description = "K3s control-manager arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
 }
 
 variable "k3s_kube_proxy_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
+  description = "K3s kube-proxy arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
 }
 
 variable "k3s_kube_sched_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
+  description = "K3s scheduler arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true", "bind-address=0.0.0.0"]
 }
 
 variable "k3s_kube_apiserver_args" {
-  type    = list(string)
-  default = ["feature-gates=MixedProtocolLBService=true"]
+  description = "K3s api server arguments"
+  type        = list(string)
+  default     = ["feature-gates=MixedProtocolLBService=true"]
 }
 
 variable "k3s_master_node_taints" {
-  type    = list(string)
-  default = ["k3s-controlplane=true:NoExecute", "CriticalAddonsOnly=true:NoExecute"]
+  description = "K3s master taints"
+  type        = list(string)
+  default     = ["k3s-controlplane=true:NoExecute", "CriticalAddonsOnly=true:NoExecute"]
 }
 
 variable "k3s_worker_node_taints" {
-  type    = list(string)
-  default = []
+  description = "K3s worker taints"
+  type        = list(string)
+  default     = []
 }
 
 variable "k3s_master_node_labels" {
-  type    = list(string)
-  default = []
+  description = "K3s master labels"
+  type        = list(string)
+  default     = []
 }
 
 variable "k3s_worker_node_labels" {
-  type    = list(string)
-  default = ["node.kubernetes.io/worker=true"]
+  description = "K3s worker labels"
+  type        = list(string)
+  default     = ["node.kubernetes.io/worker=true"]
 }
 
 variable "k3s_network_policy_disable" {
-  type    = bool
-  default = false
+  description = "K3s default network policy controller configuration"
+  type        = bool
+  default     = false
 }
 
 variable "k3s_cloud_controller_disable" {
-  type    = bool
-  default = true
+  description = "K3s default network cloud controller configuration"
+  type        = bool
+  default     = true
 }
 
 variable "k3s_kube_proxy_disable" {
-  type    = bool
-  default = false
+  description = "K3s default kube-proxy configuration"
+  type        = bool
+  default     = false
 }
 
 variable "k3s_secrets_encryption_enable" {
-  type    = bool
-  default = true
+  description = "K3s default secrets encryption configuration"
+  type        = bool
+  default     = true
 }
 
 variable "k3s_write_kubeconfig_mode" {
-  type    = string
-  default = "600"
+  description = "K3s default kube-config configuration"
+  type        = string
+  default     = "600"
 }
 
 variable "k3s_cluster_cidr" {
-  type    = string
-  default = "10.86.0.0/16"
+  description = "K3s cluster pod cidr configuration"
+  type        = string
+  default     = "10.86.0.0/16"
 }
 
 variable "k3s_service_cidr" {
-  type    = string
-  default = "10.88.0.0/16"
+  description = "K3s cluster service cidr configuration"
+  type        = string
+  default     = "10.88.0.0/16"
 }
 
 variable "k3s_sans" {
-  type    = list(string)
-  default = ["k8s.local"]
+  description = "K3s default certificate included entries configuration"
+  type        = list(string)
+  default     = ["k8s.local"]
 }
 
 variable "k3s_flannel_backend" {
-  type    = string
-  default = "vxlan"
+  description = "K3s default network backend configuration"
+  type        = string
+  default     = "vxlan"
 }
 
 variable "k3s_disable" {
-  type    = list(string)
-  default = ["traefik", "servicelb", "metrics-server", "local-storage"]
+  description = "K3s addons disable configuration"
+  type        = list(string)
+  default     = ["traefik", "servicelb", "metrics-server", "local-storage"]
 }
 
 variable "k3s_worker_protect_kernel_defaults" {
-  type    = bool
-  default = false
+  description = "K3s protect kernel defaults configuration"
+  type        = bool
+  default     = false
 }
 
 
 variable "k3s_worker_snapshotter" {
-  type    = string
-  default = "native"
+  description = "K3s default snapshotter configuration"
+  type        = string
+  default     = "native"
 }
 
 variable "private_ssh_key" {
